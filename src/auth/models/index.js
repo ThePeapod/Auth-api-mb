ee9a250d5f600ba945903ad74fbf68c3f3d56257
Brandon Pitts <Brandomoki.bp@gmail.com>
2022-09-30 01:21:52 +0000

    server up

---
+++ src/auth/models/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const { Sequelize } = require('sequelize');
+
+const DATABASE_URL = process.env.DATABASE_URL || 'sqlite::memory';
+
+const sequelize = new Sequelize(DATABASE_URL);
+
+module.exports = {
+  db: sequelize,
+};
