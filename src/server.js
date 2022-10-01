ffe565a614e34306d6eb8f6c57e6ad75a3a722ed
mariaka86 <marymwihaki123@gmail.com>
2022-09-30 22:47:51 -0500

    added routes.js, user.js and updated server.js

--- src/server.js
+++ src/server.js
@@ -1,8 +1,28 @@
 'use strict';
 
-const express = require('express');
 
+// Requirements
+const express = require('express');
+const cors = require('cors');
+const logger = require('./auth/middleware/logger');
 const app = express();
+const morgan = require('morgan');
+
+
+// Uses
+app.use(cors());
+app.use(morgan('dev'));
+app.use(authRoutes);
+app.use(logger);
+app.use('*', notFound);
+app.use(errorHandler);
+
+
+// Routes
+const errorHandler = require('./error-handlers/500.js');
+const notFound = require('./error-handlers/404.js');
+const authRoutes = require('./auth/route/routes.js');
+
 
 
 module.exports = {    //imported************************
