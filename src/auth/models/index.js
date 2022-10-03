bec71dd4725015e51f3ef85270f15daee5fe180a
Brandon Pitts <Brandomoki.bp@gmail.com>
2022-10-03 04:23:29 +0000

    all routes complete

--- src/auth/models/index.js
+++ src/auth/models/index.js
@@ -1,11 +1,20 @@
 'use strict';
 
-const { Sequelize } = require('sequelize');
+const userModel = require('./users.js');
+const { Sequelize, DataTypes } = require('sequelize');
+const animalModel = require('./animals/model.js');
+const Collection = require('./data-collection.js');
+const cetaceanModel = require('./cetacean/model.js');
 
 const DATABASE_URL = process.env.DATABASE_URL || 'sqlite::memory';
 
 const sequelize = new Sequelize(DATABASE_URL);
+const animal = animalModel(sequelize, DataTypes);
+const cetaceans = cetaceanModel(sequelize, DataTypes);
 
 module.exports = {
   db: sequelize,
+  users: userModel(sequelize, DataTypes),
+  animal: new Collection(animal),
+  cetaceans: new Collection(cetaceans),
 };
